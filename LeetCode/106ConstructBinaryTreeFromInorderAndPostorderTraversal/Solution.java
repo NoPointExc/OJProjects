53abd6aaed1dbdd9e38f7cf52bdf2f40cef4ddcc
Sun Jiayang <a13813258259@163.com>
2016-01-23 17:14:30 -0500

    heavy snow

--- LeetCode/106ConstructBinaryTreeFromInorderAndPostorderTraversal/Solution.java
+++ LeetCode/106ConstructBinaryTreeFromInorderAndPostorderTraversal/Solution.java
@@ -1,14 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
 public class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        Queue<>
+        int len=postorder.length;
+        if(len==0) return null;
+        TreeNode root=new TreeNode(postorder[len-1]);
+        LinkedList<TreeNode> stack=new LinkedList<TreeNode>();
+        stack.push(root);
+        int j=len-1;
+        for(int i=len-2;i>=0;i--){
+            TreeNode cur=stack.peek();
+            if(cur.val!=inorder[j]){
+                cur.right=new TreeNode(postorder[i]);
+                stack.push(cur.right);
+            }else{
+                while(!stack.isEmpty()   && stack.peek().val==inorder[j]){
+                    j--;
+                    cur=stack.pop();
+                }
+                if(j>=0){
+                    cur.left=new TreeNode(postorder[i]);
+                    stack.push(cur.left);    
+                }
+            }
+        }
+        return root;
     }
 }
