cef2fe8f15227fd78f77cfeeaf18073d69a7b069
jiayang,sun <a13813258259@163.com>
2016-04-04 00:29:08 +0800

    update tools for build & print binary tree

---
+++ LeetCode/Tools/Test.java
@@ -0,0 +1,17 @@
+
+class Test{
+	public static void main(String[] args) {
+		String str="[1,-2,-3,1,3,-2,null,-1]";
+		TreeNode root = TreeBuilder.create(str);
+		// TreeNode root=new TreeNode(1);
+		// TreeNode left=new TreeNode(-2);
+		// TreeNode right=new TreeNode(-3);
+		// left.left=new TreeNode(1);
+		// left.right=new TreeNode(3);
+		// right.right=new TreeNode(-2);
+		// left.left.left=new TreeNode(-1);
+		// root.left=left;
+		// root.right=right;
+		TreeBuilder.print(root);
+	}
+}
