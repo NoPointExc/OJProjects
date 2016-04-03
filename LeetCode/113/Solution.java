8bcefb6b8d497c92f8f9b81f6af1987c4d22dcb8
jiayang,sun <a13813258259@163.com>
2016-04-04 01:47:32 +0800

    recurision solution

--- LeetCode/113/Solution.java
+++ LeetCode/113/Solution.java
@@ -1,35 +1,23 @@
 import java.util.*;
 
 public class Solution {
+	private LinkedList<List<Integer>> list=new LinkedList<List<Integer>>();
+	
+	public List<List<Integer>> pathSum(TreeNode root, int sum) {
+    	pathSum(root,sum,new LinkedList<Integer>());
+    	return list;
+	}
 
-    public List<List<Integer>> pathSum(TreeNode root, int sum) {
-		LinkedList<List<Integer>> list=new LinkedList<List<Integer>>();
-		LinkedList<Integer> pthList=new LinkedList<Integer>();
-		int curSum=0; //the sum of the nodStack
-		LinkedList<TreeNode> nodStack=new LinkedList<TreeNode>();
-
-		if(root==null) return list;
-		TreeNode cur=root;
-		while(cur!=null||!nodStack.isEmpty()){
-			if(cur!=null){
-			//go left
-				nodStack.push(cur);
-				curSum+=cur.val;
-				pthList.add(cur.val);
-				cur=cur.left;
-			}else{				
-				cur=nodStack.pop();					
-				if(cur.left==null && cur.right==null){					
-					//System.out.print("sum="+curSum);
-					System.out.print("*");																						
-				}
-				System.out.println(cur.val);
-				//System.out.print(nodStack);				
-				//go right
-				cur=cur.right;														
-			}
-		}
-		return list;
+    private void pathSum(TreeNode root, int sum,List<Integer> pth) {    	   	
+    	if(root==null) return;
+    	int val=root.val;    	
+    	pth.add(val);	
+    	if(root.left==null && root.right==null){
+    		if(sum==val) list.add(pth);
+    		return;
+    	}
+    	pathSum(root.left,sum-val,new LinkedList<Integer>(pth));
+    	pathSum(root.right,sum-val,new LinkedList<Integer>(pth));
     }
     
 }
--- LeetCode/113/Test.java
+++ LeetCode/113/Test.java
@@ -2,17 +2,11 @@ import java.util.*;
 class Test{
 	public static void main(String[] args) {
 		Solution s=new Solution();
-		TreeNode root=new TreeNode(1);
-		TreeNode cur=root;
-		cur.left=new TreeNode(2);
-		cur.right=new TreeNode(3);
-		cur=cur.left;
-		cur.right=new TreeNode(4);
-		List<List<Integer>> ans=s.pathSum(root,1);
-		//System.out.println(ans);
+		String str="[1,2,3,4]";
+		TreeNode root = TreeBuilder.create(str);		
+		TreeBuilder.print(root);
+		
+		List<List<Integer>> ans=s.pathSum(root,4);
+		System.out.println(ans);
 	}
 }
-
-          //   1
-          // 2     3
-          //   4
