a3cc94217cdb6a9ff12e20c5900ff90cf4dd9177
jiayang,sun <a13813258259@163.com>
2016-04-06 22:18:06 +0800

    add postorder inorder preorder in tool

--- LeetCode/113pathSum/Solution.java
+++ LeetCode/113pathSum/Solution.java
@@ -1,23 +1,14 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-	private LinkedList<List<Integer>> list=new LinkedList<List<Integer>>();
-	
-	public List<List<Integer>> pathSum(TreeNode root, int sum) {
-    	pathSum(root,sum,new LinkedList<Integer>());
-    	return list;
-	}
-
-    private void pathSum(TreeNode root, int sum,List<Integer> pth) {    	   	
-    	if(root==null) return;
-    	int val=root.val;    	
-    	pth.add(val);	
-    	if(root.left==null && root.right==null){
-    		if(sum==val) list.add(pth);
-    		return;
-    	}
-    	pathSum(root.left,sum-val,new LinkedList<Integer>(pth));
-    	pathSum(root.right,sum-val,new LinkedList<Integer>(pth));
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        
     }
-    
 }
--- LeetCode/Tools/Test.java
+++ LeetCode/Tools/Test.java
@@ -1,8 +1,10 @@
+import java.util.*;
 
 class Test{
 	public static void main(String[] args) {
 		String str="[1,-2,-3,1,3,-2,null,-1]";
 		TreeNode root = TreeBuilder.create(str);
+		//TreeNode root=null;
 		// TreeNode root=new TreeNode(1);
 		// TreeNode left=new TreeNode(-2);
 		// TreeNode right=new TreeNode(-3);
@@ -13,5 +15,11 @@ class Test{
 		// root.left=left;
 		// root.right=right;
 		TreeBuilder.print(root);
+		Traveler trv=new Traveler();
+		//trv.postorder(root);
+		//trv.perorder(root);
+		trv.inorder(root);
+		//List<List<Integer>>ans=trv.levelorder(root);
+
 	}
 }
