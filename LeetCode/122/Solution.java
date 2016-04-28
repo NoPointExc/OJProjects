20c496e352df9dbe80626b5305a48b9ef9104eac
Sun Jiayang <a13813258259@163.com>
2016-04-28 23:14:55 +0800

    122 AC

--- LeetCode/122/Solution.java
+++ LeetCode/122/Solution.java
@@ -1,31 +1,20 @@
 public class Solution {
-    int[] prices;
-        public int maxProfit(int[] prices) {
-	        int len=prices.length;
-		        int[][] prof=new int[len][len];
-			        if(len==0) return 0;
-				        for(int i=0;i<len;i++){
-					            for(int j=0;j<len;j++){
-						                    if(j>=i){
-								                        prof[i][j]=0;
-											                }else{
-													                    prof[i][j]=prices[j]-prices[i];
-															                    }
-																	                }
-																			        }
-																				        
-																					        for(int i=1;i<len;i++){
-																						            prof[i][0]=prof[i][0]>prof[i-1][0]?prof[i][0]:prof[i-1][0];
-																							                for(int j=1;j<i;j++){
-																									                int tmp=prof[i-1][j-1]>prof[i-1][j]?prof[i-1][j-1]:prof[i-1][j];
-																											                prof[i][j]=tmp>prof[i][j]?tmp:prof[i][j];
-																													            }
-																														            }
-																															            
-																																            int max=prof[len-1][0];
-																																	            for(int i=1;i<len;i++){
-																																		                max=prof[len-1][i]>max?prof[len-1][i]:max;
-																																				        }
-																																					        return max;
-																																						    }
-																																						    }
+    public int maxProfit(int[] prices) {
+        int prof=0;
+        if(prices==null||prices.length<2) return 0;
+        int min=prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<min){
+                    min=prices[i];
+            }else{
+                if(i<prices.length-1&&prices[i]<prices[i+1]){
+                    continue;
+                }else{
+                    prof+=prices[i]-min;
+                    if(i<prices.length-1) min=prices[i+1];
+                }
+            }
+        }
+        return prof;
+    }
+}
