fefac5169eacf7efab6ecff75cc3a1d20d171bd6
jiayang,sun <jiayangs@intel.com>
2016-05-15 05:01:35 +0800

    1) 2008A1-B iteration solution 2) without wqadding no solution case

---
+++ codeJam/2008A1/B.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+
+public class B {
+	//private static String fileName = "B-large-practice";
+	private static String fileName = "B-small-practice";	
+	private static String inputFileName = fileName + ".in";
+	private static String outputFileName = fileName + ".out";
+	private static Scanner in;
+	private static PrintWriter out;
+	
+	int[][] choice;
+	int cust;
+	private void solve() {
+		int flavor=in.nextInt();
+		cust=in.nextInt();
+		boolean[] isHappy=new boolean[cust];
+		Arrays.fill(isHappy,false);
+		int[] milk=new int[flavor];
+		Arrays.fill(milk,0);
+		choice=new int[flavor][cust];
+		for(int i=0;i<flavor;i++){
+			Arrays.fill(choice[i],-1);//-1 don't care 
+		}
+		//get solution
+		int rst=minMalted(0,milk,isHappy);
+		if(rst==-1){
+			out.println("IMPOSSIBLE");
+		}else{
+			for(int i=0;i<flavor;i++){
+				out.print(milk[i]+" ");
+			}
+			out.println();
+		}			
+	}
+
+	private int minMalted(int from,int[] milk,boolean[] isHappy){
+		if(from==milk.length){
+			return 0;
+		}
+		boolean mayMalted=false;
+		
+		//UN-Malted
+		boolean[] isHappyTmp=Array.copyOf(isHappy,isHappy.length);
+		int[] milkTmp=Array.copyOf(milk,milk.length);
+		//ask every customer && try unmalted
+		for(int i=0;i<cust;i++){
+			//not happy & like malted
+			if(!isHappy[i]){
+				if(choice[from,i]==0){
+					isHappyTmp[i]=true;
+				}else{
+					mayMalted=true;
+				}
+			}
+		}
+		int[] milkTmp=Array.copyOf(milk,milk.length);
+		int rst=minMalted(from+1,milkTmp,isHappyTmp);
+		
+		//MALTED
+		if(maymalted){
+			int[] milkMal=Array.copyOf(milk,milk.length);
+			milkMal[from]=1;
+			boolean isHappyMal[]=Array.copyOf(isHappy,isHappy.length);
+			//ask every customer if become happy
+			for(int i=0;i<isHappy.length;i++){
+				if(!isHappy[i]&&choice[from][i]==1){
+					isHappyMal[i]=true;
+				}
+			}
+			int rstMal=minMalted(from+1,milkMal,isHappyMal);
+			rst=rst<rstMal?rst:rstMal;
+		}
+		
+		return rst;	
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		Locale.setDefault(Locale.US);
+		if (args.length >= 2) {
+			inputFileName = args[0];
+			outputFileName = args[1];
+		}
+		in = new Scanner(new FileReader(inputFileName));
+		out = new PrintWriter(outputFileName);
+		int tests = in.nextInt();
+		in.nextLine();
+		for (int t = 1; t <= tests; t++) {
+			out.print("Case #" + t + ": ");
+			new B().solve();
+		}
+		in.close();
+		out.close();
+	}
+}
