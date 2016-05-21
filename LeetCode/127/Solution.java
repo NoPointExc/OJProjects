0819219ceb4d7f4b95a742a0c4e136414d3edb4f
Sun Jiayang <a13813258259@163.com>
2016-05-21 23:39:20 +0800

    1> search char 'a'~'b' is much shorter than long arr of dict

--- LeetCode/127/Solution.java
+++ LeetCode/127/Solution.java
@@ -1,48 +1,28 @@
-import java.util.*;
-
-public class Solution {
-    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
-        if(beginWord.equals(endWord)) return 1;
-        if(beginWord.length()==1) return 2;
-        int len=wordList.size();
-        LinkedList<String> from=new LinkedList<String> ();
-        from.add(beginWord);
-        LinkedList<String> to=new LinkedList<String>();
-        int step=1;
-        int oneMore=2;
-        while(oneMore>0){
-        		if(from.isEmpty()) oneMore--;
-        		step++;
-                for(String begin:from){                    
-                    System.out.println("[before]step="+step+" begin="+begin+" from="+from+" wordList"+wordList);
-                    if(isOneStep(begin,endWord)) return step;
-                    Iterator<String> it=wordList.iterator();
-                    while(it.hasNext()){
-                        String word=it.next();
-                        if(word.equals(begin)||word.equals(endWord)){
-                            it.remove();
-                        }else if(isOneStep(begin,word)){
-                            to.add(word);
-                            it.remove();
-                        }   
+class Solution{
+    public int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
+        Set<String> reached = new HashSet<String>();
+        reached.add(beginWord);
+        wordDict.add(endWord);
+        int distance = 1;
+        while(!reached.contains(endWord)) {
+            Set<String> toAdd = new HashSet<String>();
+            for(String each : reached) {
+                for (int i = 0; i < each.length(); i++) {
+                    char[] chars = each.toCharArray();
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        chars[i] = ch;
+                        String word = new String(chars);
+                        if(wordDict.contains(word)) {
+                            toAdd.add(word);
+                            wordDict.remove(word);
+                        }
                     }
-         
-	            }
-				from=to;
-				to=new LinkedList<String>();   
-        }
-
-        return 0;
-    }
-    
-    private boolean isOneStep(String from, String to){
-        int ct=0;
-        int len=from.length();
-        for(int i=0;i<len;i++){
-            if(from.charAt(i)!=to.charAt(i)) {
-                ct++;
+                }
             }
+            distance ++;
+            if(toAdd.size() == 0) return 0;
+            reached = toAdd;
         }
-        return ct==1;
+        return distance;
     }
 }
