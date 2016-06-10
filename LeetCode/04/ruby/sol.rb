f60a9ce4f9d03771dd5fb6a707b35699e80b80db
jiayang,sun <a13813258259@163.com>
2016-06-11 01:05:28 +0800

    Leetcode 04 mid of two sorted arr: AC

--- LeetCode/04/ruby/sol.rb
+++ LeetCode/04/ruby/sol.rb
@@ -2,6 +2,31 @@
 # @param {Integer[]} nums2
 # @return {Float}
 def find_median_sorted_arrays(nums1, nums2)
-   int len=nums1.length + nums2.length
-    
+   	len=nums1.length + nums2.length
+       	p1=0
+	p2=0
+	half=len/2   
+	mid1=nil
+	mid2=nil
+	while (p1+p2) <= half do
+		mid2=mid1
+		if nums2[p2]==nil || (nums1[p1]!=nil && nums1[p1]<=nums2[p2])
+			mid1 = nums1[p1]
+			p1+=1		
+		else
+			mid1 = nums2[p2]
+			p2+=1
+		end	
+	end
+	
+	rst=mid1
+	if len%2==0
+		rst= (mid1+mid2) / 2.0
+	end
+	return rst	
 end
+
+#test
+nums1=[3,8,9]
+nums2=[1,2,4,5]
+puts find_median_sorted_arrays nums1 , nums2 
