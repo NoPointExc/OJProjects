c572ea285afa8aeaffde3207da03b7207759ce5a
Jiayang, Sun <Jiayang.sun@intel.com>
2016-06-23 02:54:07 +0800

    132:AC
    use charArray instand of substring.  AC but slow

--- LeetCode/132/Solution.java
+++ LeetCode/132/Solution.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
 class Solution {
-	private HashMap<String,Boolean> map=new HashMap();
-
+	//private HashMap<String,Boolean> map=new HashMap();
+	char[] str;
 	public int minCut(String s) {
 		if(s==null) return 0;
 		int len=s.length();
-		if(len<=1 || isPalindrome(s)) return 0;
+		str=s.toCharArray();
+		if(len<=1 || isPalindrome(0,len)) return 0;
 		int[] cuts=new int[len];
 		Arrays.fill(cuts,len-1);
 		
 		for(int i=len-1;i>=0;i--){
-			if(isPalindrome(s.substring(i))){
+			if(isPalindrome(i,len)){
 				cuts[i]=0;
 			}else{
 				for(int j=i+1;j<len;j++){
-					if(cuts[j]+1<cuts[i] && isPalindrome(s.substring(i,j)))
+					if(cuts[j]+1<cuts[i] && isPalindrome(i,j))
 						cuts[i]=1+cuts[j];
 				}
 			}
@@ -24,16 +25,15 @@ class Solution {
 		return cuts[0];	
 	}
 
-	private boolean isPalindrome(String str){
-		if(map.containsKey(str)) return map.get(str);
-		int len=str.length();
-		for(int i=0;i<(len-i-1);i++){
-			if( str.charAt(i)!=str.charAt(len-i-1) ) {
-				map.put(str,false);
+	private boolean isPalindrome(int from, int to){
+		//if(map.containsKey(str)) return map.get(str);
+		for(int i=0;(from+i)<(to-i-1);i++){
+			if( str[from+i]!=str[to-i-1] ) {
+				//map.put(str,false);
 				return false;
 			}
 		}
-		map.put(str,true);
+		//map.put(str,true);
 		return true;
 	}
 }
