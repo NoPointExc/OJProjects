7cfa63ab8781fdd1b2d8df3b4b4d317924bc443e
Jiayang, Sun <Jiayang.sun@intel.com>
2016-06-23 01:39:14 +0800

    trim a TLE

--- LeetCode/132/Solution.java
+++ LeetCode/132/Solution.java
@@ -2,31 +2,25 @@ import java.util.*;
 
 class Solution {
 	private HashMap<String,Boolean> map=new HashMap();
-	
+
 	public int minCut(String s) {
-		//System.out.println(s);
 		if(s==null) return 0;
 		int len=s.length();
 		if(len<=1 || isPalindrome(s)) return 0;
-		
 		int[] cuts=new int[len];
 		Arrays.fill(cuts,len-1);
+		
 		for(int i=len-1;i>=0;i--){
-			//System.out.println("*"+i);
 			if(isPalindrome(s.substring(i))){
 				cuts[i]=0;
-				continue;
-			}
-			for(int j=i+1;j<=len;j++){
-				if(j!=len && cuts[j]+1<cuts[i] && isPalindrome(s.substring(i,j))){
-					int tmp=1+cuts[j];
-					cuts[i]=cuts[i]<tmp?cuts[i]:tmp;
-					//System.out.println("i="+i+" j="+j);
+			}else{
+				for(int j=i+1;j<len;j++){
+					if(cuts[j]+1<cuts[i] && isPalindrome(s.substring(i,j)))
+						cuts[i]=1+cuts[j];
 				}
 			}
 		}
-		
-		//for(int i=0;i<len;i++) System.out.print(" "+cuts[i]+",");
+
 		return cuts[0];	
 	}
 
