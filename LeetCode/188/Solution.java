7d761aa2947fa3a40facb80c8905a84a79d487b7
Sun Jiayang <a13813258259@163.com>
2016-08-03 22:46:21 +0800

    188 AC

--- LeetCode/188/Solution.java
+++ LeetCode/188/Solution.java
@@ -1,49 +1,25 @@
-import java.util.*;
-
 public class Solution {
-    private int[] prices;
-    int len;
-    
     public int maxProfit(int k, int[] prices) {
-        this.prices=prices;
-        len=prices.length;
-        return maxProfit(k,-1,0);
+        int len = prices.length;
+        if (k >= len / 2) return quickSolve(prices);
+        
+        int[][] t = new int[k + 1][len];
+        for (int i = 1; i <= k; i++) {
+            int tmpMax =  -prices[0];
+            for (int j = 1; j < len; j++) {
+                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
+                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
+            }
+        }
+        return t[k][len - 1];
     }
+    
 
-    private int maxProfit(int k,int min,int from){
-    	//System.out.println("k="+k+" min="+min+" from="+from);
-    	if(from>=len || k==0 ) return 0;
-
-    	if(min==-1) min=prices[from];
-    	int max=0;
-    	int i=from;    	
-    	
-    	if(i+1<len && prices[i]<prices[i+1]){
-	    	//while rising
-	    	if(prices[i]<min) min=prices[i];
-	    	while(i+1<len && prices[i]<prices[i+1]){
-	    		i++;
-	    	}
-
-	    	if(prices[i]>min){
-	    		//sell out
-	    		int sell=prices[i]-min+maxProfit(k-1,-1,i);
-	    		int keep=maxProfit(k,min,i);
-	    		max=Math.max(sell,keep);
-	    	}else{
-	    		max=maxProfit(k,min,i+1);
-	    	}
-    	}else if(i+1<len){
-    		//while descending
-    		while(i+1<len && prices[i]>=prices[i+1]){
-    			i++;
-    		}
-    		if(prices[i]<min) min=prices[i];
-    		max=maxProfit(k,min,i);
-    	}else{
-    		max=maxProfit(k,min,i+1);
-    	}
-
-    	return max;
+    private int quickSolve(int[] prices) {
+        int len = prices.length, profit = 0;
+        for (int i = 1; i < len; i++)
+            // as long as there is a price gap, we gain a profit.
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        return profit;
     }
 }
