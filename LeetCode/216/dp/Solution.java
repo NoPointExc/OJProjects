61b1f8c58dbb2657aa2c00c5908a19e0d5fb1c12
jiayang,sun <a13813258259@163.com>
2016-08-25 02:35:03 +0800

    216 dp AC

--- LeetCode/216/dp/Solution.java
+++ LeetCode/216/dp/Solution.java
@@ -6,16 +6,29 @@ public class Solution {
             return new LinkedList<List<Integer>>();
         List<List<Integer>>[][] dp = new List[k+1][n+1];
         //init 0 line and 0 col
-        for(int i=0;i<=n;i++)
-            dp[0][i] = new LinkedList<List<Integer>>();
-        for(int i=1;i<=k;i++)
-            dp[i][0] = new LinkedList<List<Integer>>();
+        //for(int i=0;i<=n;i++)
+        //   dp[0][i] = new LinkedList<List<Integer>>();
+        //for(int i=1;i<=k;i++)
+        //   dp[i][0] = new LinkedList<List<Integer>>();
+
+        for(int i=0;i<=k;i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j]=new LinkedList<List<Integer>>();
+            }
+        }
+        //init line 1
+        for(int i=1;i<=9&&i<=n;i++){
+            LinkedList<Integer> tmp=new LinkedList<Integer>();
+            tmp.add(i);
+            dp[1][i]=new LinkedList<List<Integer>>();
+            dp[1][i].add(tmp);
+        }
 
         //fill up do table
-        for(int i=1;i<=k;i++){
+        for(int i=2;i<=k;i++){
             for(int j=1;j<=n;j++){
-                for(int head=i;head>n-head && head<=9;head++){
-                    List<List<Integer>> lastRst = dp[i-1][n-head];
+                for(int head=i;j-head>=1 && head<=9;head++){
+                    List<List<Integer>> lastRst = dp[i-1][j-head];
                     List<List<Integer>> tmp = addHead(head,lastRst);
                     dp[i][j].addAll(tmp);
                 }
@@ -31,7 +44,7 @@ public class Solution {
         for(List<Integer> l:lastRst){
             if(l.get(0)<head){
                 ArrayList<Integer> tmp = new ArrayList<Integer>(l);
-                tmp.add(head);
+                tmp.add(0,head);
                 rst.add(tmp); 
             }
         }
