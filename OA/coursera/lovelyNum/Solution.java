3ecde4075846cade1732976f4344191fd4f1daf1
Sun Jiayang <a13813258259@163.com>
2016-09-12 21:00:07 -0400

    54/59 round2 AC
    59 have much more readable solution

--- OA/coursera/lovelyNum/Solution.java
+++
@@ -1,6 +0,0 @@
-class Solution{
-    public static void main(String[] args){
-	System.out.println(1234567^1111111);
-	System.out.println(1234567^2222222);
-    }
-}
--- OA/coursera/lovelyNum/readme.md
+++
@@ -1,2 +0,0 @@
-No repeat digit in a number is a lovely num.
-Please write a function to decide whether a num is lovely. The fucntion should run in O(1) time.
