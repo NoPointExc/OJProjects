86dfe6cafaed72c8c1d8e67a896cf12e3f64bf09
Sun Jiayang <a13813258259@163.com>
2016-10-21 14:34:02 -0400

    do not known why

---
+++ LeetCode/360/other/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+        double piv = -b / 2.0 / a;
+        int n = nums.length;
+        boolean direction = a < 0 || (a == 0 && b > 0);
+        int[] ans = new int[n];
+        for (int k = 0, i = 0, j = n - 1; k < n; k++)
+            ans[direction ? k : n - k - 1] = cal(nums[(Math.abs(nums[j] - piv) > Math.abs(nums[i] - piv) ? j-- : i++)], a, b, c);
+        return ans;
+    }
+    
+    private int cal(int x, int a, int b, int c) {
+        return a * x * x + b * x + c;
+    }
+}
