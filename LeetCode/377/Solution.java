1317b8f2048a5b8691d6026f8d06aaf62e668814
Sun Jiayang <a13813258259@163.com>
2016-11-06 10:01:28 -0500

    377 AC

--- LeetCode/377/Solution.java
+++ LeetCode/377/Solution.java
@@ -1,28 +1,68 @@
-import java.util.*;
 public class Solution {
-	HashMap<Integer, Integer> map = new HashMap<>();
-
-    public int combinationSum4(int[] nums, int target) {
-        if(nums == null || target <= 0){
-        	return 0;
+    public List<int[]> getSkyline(int[][] buildings) {
+        if(buildings == null){
+            return null;
         }
-        if(map.containsKey(target)){
-        	return map.get(target);
+        //get critical points
+        Point[] points = new Point[2 * buildings.length];
+        for(int i = 0; i < buildings.length; i++){
+            points[2 * i] = new Point(buildings[i][0], buildings[i][2], false);
+            points[2 * i + 1] = new Point(buildings[i][1], buildings[i][2], true);
         }
-        int count = 0;
-        for(int n : nums){
-        	if(n < target){
-        		count += combinationSum4(nums, target - n);
-        	}else if(n == target){
-        		count += 1;
-        	}
+        
+        //sort points
+        Arrays.sort(points, new Comparator<Point>(){
+            @Override
+            public int compare(Point p1, Point p2){
+                return p1.x - p2.x;
+            }
+        });
+        
+        
+        //height queue
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        queue.offer(0);
+        
+        //go through points, skip overlap x .
+        List<int[]> list = new LinkedList<>();
+        int preHeight = 0;
+        for(int i = 0; i < points.length; i++){
+            //System.out.println(queue);
+            //System.out.println(points[i]);
+            
+            if(!points[i].isExit){
+                queue.offer(points[i].y);
+            }else{
+                queue.remove(points[i].y);
+            }
+            
+            //jump over overlap x points
+            if(i + 1 < points.length && points[i + 1].x == points[i].x){
+                continue;
+            }
+           
+            if(preHeight != queue.peek()){
+                //height change. add to rst list
+                list.add(new int[]{points[i].x, queue.peek()});
+            }
+            preHeight = queue.peek();
         }
-        map.put(target, count);
-        return count;
+        
+        return list;
     }
-
-    public static void main(String[] args) {
-    	Solution sol = new Solution();
-    	System.out.println(sol.combinationSum4(new int[]{1,2,3}, 4));
+    
+    class Point{
+        int x, y;
+        boolean isExit;
+        public Point(int x, int y, boolean isExit){
+            this.x = x;
+            this.y = y;
+            this.isExit = isExit;
+        }
+        
+        @Override
+        public String toString(){
+            return "[" + x + "," + y + "," + (isExit? "e" : "s")+"]";
+        }
     }
 }
